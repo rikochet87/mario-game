7fb26c076c73cef12d9e205e32c221e79a334a2a
matias <matias3282@hotmail.com>
2022-07-16 11:15:53 -0300

    player movement

--- src/index.js
+++ src/index.js
@@ -6,13 +6,77 @@ const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const player = new Player()
+const player = new Player();
+const keys = {
+  right: {
+    pressed : false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
-function animate(){
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if(keys.right.pressed){
+      player.velocity.x = 5
+  }else if(keys.left.pressed){
+      player.velocity.x = -5
+  } else player.velocity.x = 0
 }
-animate()
+animate();
 
-export {c, canvas}
+(function () {
+  addEventListener("keydown", ({ keyCode }) => {
+    switch (keyCode) {
+      case 65:
+        console.log("left");
+        keys.left.pressed = true;
+        break;
+
+      case 83:
+        console.log("down");
+        break;
+
+      case 68:
+        console.log("right");
+        keys.right.pressed = true;
+        break;
+
+      case 87:
+        console.log("up");
+        player.velocity.y -= 20;
+        break;
+    }
+  });
+})();
+
+(function () {
+  addEventListener("keyup", ({ keyCode }) => {
+    switch (keyCode) {
+      case 65:
+        console.log("left");
+        keys.left.pressed = false;
+        break;
+
+      case 83:
+        console.log("down");
+        break;
+
+      case 68:
+        console.log("right");
+        keys.right.pressed = false;
+        break;
+
+      case 87:
+        console.log("up");
+        player.velocity.y = 0;
+        break;
+    }
+  });
+})();
+
+export { c, canvas };
--- src/player.js
+++ src/player.js
@@ -22,6 +22,8 @@ class Player {
   update() {
     this.draw();
     this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+
     if (this.position.y + this.height + this.velocity.y <= canvas.height)
       this.velocity.y += gravity;
       else this.velocity.y = 0;
